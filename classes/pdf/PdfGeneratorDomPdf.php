bdcfb6cbe7eb5122fc3ff0fb75f05ce2812c787f
Patrick Froch <patrick.froch@easySolutionsIT.de>
2016-12-19 11:57:56 +0100

    FIXED: Setzen des Default-Dateinamens im Konstruktor von PdfGeneratorDomPdf

--- classes/pdf/PdfGeneratorDomPdf.php
+++ classes/pdf/PdfGeneratorDomPdf.php
@@ -36,7 +36,7 @@ class PdfGeneratorDomPdf extends PdfGeneratorGeneric
         }
 
         if(isset($GLOBALS['c4g']['projects']['pdf']['dompdf']['defaultfilename'])) {
-            $this->getFilename($GLOBALS['c4g']['projects']['pdf']['dompdf']['defaultfilename']);
+            $this->setFilename($GLOBALS['c4g']['projects']['pdf']['dompdf']['defaultfilename']);
         }
     }
 
