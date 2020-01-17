d24e5a7d24fe01f69d1363850fc32f0a93885ea1
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2020-01-17 17:08:22 +0100

    changed all be icons to svg

--- Resources/contao/dca/tl_c4g_documents_pdfstack.php
+++ Resources/contao/dca/tl_c4g_documents_pdfstack.php
@@ -51,7 +51,8 @@ $GLOBALS['TL_DCA'][$strName] = array
 			'mode'                    => 1,
 			'fields'                  => array('id'),
             'panelLayout'             => 'sort,filter;search,limit',
-			'flag'                    => 1
+			'flag'                    => 1,
+            'icon'                    => 'bundles/con4giscore/images/be-icons/con4gis.org_dark.svg',
 		),
 		'label' => array
 		(
@@ -99,7 +100,7 @@ $GLOBALS['TL_DCA'][$strName] = array
 			(
 				'label'               => &$GLOBALS['TL_LANG'][$strName]['show'],
 				'href'                => 'act=show',
-				'icon'                => 'show.gif'
+				'icon'                => 'show.svg'
 			)
 		)
 	),
